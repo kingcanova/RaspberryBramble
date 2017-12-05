fe5a698af990233604fefe554cbbb474ba41340a
KingCanova <tcanova1@gmail.com>
2017-12-05 15:24:58 -0500

    Found the bug and squashed the everliving shit out of it, it wasnt a bug it was my test file :-(

--- palindrome_cluster_compute.py
+++ palindrome_cluster_compute.py
@@ -9,21 +9,23 @@ def setup(data_file):
     i = 0
     nextUpper = 'A'
     nextLower = 'a'
-    for line in f:
-        line = str(line)
-        lines = line.split('\n')
-        line = lines[0]
-        print("got here")
-        # ord()#turns char to ascii value
-        # chr()#turns number to ascii character
-        words.append(line)
-        if (words[i][0] == nextUpper or words[i][0] == nextLower):
+    with open(data_file) as fp:
+        for line in fp:
+           line = str(line)
+           lines = line.split('\n')
+           line = lines[0]
+           #print("got here")
+           # ord()#turns char to ascii value
+           # chr()#turns number to ascii character
+           words.append(line)
+           if (words[i][0] == nextUpper or words[i][0] == nextLower):
 
-            nextUpper = chr(ord(nextUpper)+ 1)
-            nextLower = chr(ord(nextLower)+ 1)
-            bounds.append(i)
-        i += 1
-        print("got here2")
+              nextUpper = chr(ord(nextUpper)+ 1)
+              nextLower = chr(ord(nextLower)+ 1)
+              bounds.append(i)
+           i += 1
+    #print("got here2")
+    bounds.append(i)
     return 0
 
 
@@ -44,14 +46,13 @@ def compute(bIndex):
         word = words[x]
         reverse = word[::-1]
         rChar = reverse[0]
-        if (rChar < 'a'):
-            rChar += 32
-        firstLetterId = rChar - 97
+        if (ord(rChar) < ord('a')):
+            rChar = chr(ord(rChar)+32)
+        firstLetterId = ord(rChar) - 97
         if (firstLetterId >= 0 and firstLetterId < 26):
             # for each word in the first letter group
-            j = 1
             found = 0
-            for j in range(bounds[firstLetterId], bounds[firstLetterId + 1]):
+            for j in range(bounds[firstLetterId], bounds[firstLetterId+1]):
 
                 if (words[j] == reverse):
 
@@ -66,28 +67,38 @@ def compute(bIndex):
 if __name__ == '__main__':
     # executed on client only; variables created below, including modules imported,
     # are not available in job computations
-    import dispy, sys, functools, random
+    import dispy, sys, functools, random, dispy.httpd
 
     # if no data file name is given, use this file as data file
     data_file = sys.argv[1] if len(sys.argv) > 1 else sys.exit()
     print(sys.argv[1] + "\n\n\n")
-    cluster = dispy.JobCluster(compute,nodes=['10.20.1.17','10.20.1.72','10.20.1.32','10.20.1.75','10.20.0.73'], depends=[data_file],
+    cluster = dispy.JobCluster(compute,nodes=['10.20.1.17','10.20.1.72','10.20.1.32','10.20.1.75','10.20.0.73'], depends=[data_file], recover_file='crashData', loglevel=dispy.logger.INFO,
                                setup=functools.partial(setup, data_file), cleanup=cleanup)
     # run 'compute' with 20 random numbers on available CPUs
     jobs = []
-    print("PRooooblem here \n\n\n")
+    http_server = dispy.httpd.DispyHTTPServer(cluster)
+
+    # cluster can now be monitored / managed in web browser at
+    # http://<host>:8181 where <host> is name or IP address of
+    # computer running this program
+    #print("PRooooblem here \n\n\n")
     for i in range(26):
         job = cluster.submit(i)
         job.id = i  # associate an ID to identify jobs (if needed later)
         jobs.append(job)
     # cluster.wait() # waits until all jobs finish
-    print("THE PROBLEM IS IN THE JOBS LOOP BELOOOWWW \n\n\n")
+    totalPals = 0
     for job in jobs:
-        print("Is the problem in returning the job? \n\n\n")
+        #print("Is the problem in returning the job? \n\n\n")
+        words = job()
+        #print(words)
         pals, hostname = job()  # waits for job to finish and returns results
-        print("NO ITS PRINTING THE JOB \n\n\n")
-        print('%s executed job %d at %s with %d palindromes counted' % (hostname, job.id, job.start_time,pals))
+        totalPals += pals
+        print('%s executed job %s at %s with %s palindromes counted' % (hostname, job.id, job.start_time,pals))
         # other fields of 'job' that may be useful:
         # job.stdout, job.stderr, job.exception, job.ip_addr, job.end_time
+    print("\n\n")
+    print("The total number of palindromes found is: %s" % (totalPals))
     cluster.print_status()  # shows which nodes executed how many jobs etc.
+    http_server.shutdown()
     cluster.close()
--- pi_recover.py
+++ pi_recover.py
@@ -1,7 +1,7 @@
 
 import dispy
 
-jobs = dispy.recover_jobs()
+jobs = dispy.recover_jobs('crash')
 for job in jobs:
         print('Job result: %s' % job.result)
 
